9f292afc4caf3e1b55b63f67c14028a791cb9ff1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-25 15:42:51 -0500

    added controls to navigate between scenes

--- src/main/java/ca/durhamcollege/oop3200f2022week11b/Vector2CanvasController.java
+++ src/main/java/ca/durhamcollege/oop3200f2022week11b/Vector2CanvasController.java
@@ -1,5 +1,29 @@
 package ca.durhamcollege.oop3200f2022week11b;
 
-public class Vector2CanvasController
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Vector2CanvasController implements Initializable
 {
+
+    @FXML
+    private Button VectorListButton;
+
+    @FXML
+    void VectorListButton_Clicked(ActionEvent event) throws IOException
+    {
+        SceneManager.Instance().changeScene(event, "shape-view.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }
 }
