2416e460c8b7c70471ce9b6d344debefdee312c8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-25 15:56:21 -0500

    added border to canvas

--- src/main/java/ca/durhamcollege/oop3200f2022week11b/Vector2CanvasController.java
+++ src/main/java/ca/durhamcollege/oop3200f2022week11b/Vector2CanvasController.java
@@ -3,7 +3,10 @@ package ca.durhamcollege.oop3200f2022week11b;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,6 +19,9 @@ public class Vector2CanvasController implements Initializable
     private Button VectorListButton;
 
     @FXML
+    private Canvas canvas;
+
+    @FXML
     void VectorListButton_Clicked(ActionEvent event) throws IOException
     {
         SceneManager.Instance().changeScene(event, "shape-view.fxml");
@@ -24,6 +30,26 @@ public class Vector2CanvasController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        drawCanvasBorder();
+    }
+
+    private void drawCanvasBorder()
+    {
+        GraphicsContext gc = canvas.getGraphicsContext2D() ;
+        gc.setStroke(Color.BLACK);
+        gc.moveTo(0, 0);
+        gc.lineTo(canvas.getWidth(), 0 );
+        gc.stroke();
+        gc.moveTo(canvas.getWidth(), 0 );
+        gc.lineTo(canvas.getWidth(), canvas.getHeight());
+        gc.stroke();
+        gc.moveTo(canvas.getWidth(), canvas.getHeight());
+        gc.lineTo(0, canvas.getHeight());
+        gc.stroke();
+        gc.moveTo(0, canvas.getHeight());
+        gc.lineTo(0, 0);
+        gc.stroke();
+
 
     }
 }
